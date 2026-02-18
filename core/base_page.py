e2ce1e8e476f379f5d9dc50c391110d5d5e94207
daniskuvc <danisku@gmail.com>
2026-02-18 15:26:34 -0400

    Implemented explicit wait for elements

--- core/base_page.py
+++ core/base_page.py
@@ -1,12 +1,21 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class BasePage:
 
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
 
     def open(self, url):
         self.driver.get(url)
 
     def get_title(self):
         return self.driver.title
+    
+    def wait_for_element_visible(self, locator):
+        return self.wait.until(EC.visibility_of_element_located(locator))
+    
+    def wait_for_clickable(self, locator):
+        return self.wait.until(EC.element_to_be_clickable(locator))
